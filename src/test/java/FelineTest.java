4462dea0b5eac865fa51a28bb4d08487aced0c78
L1sssica <lida_shalygina01@mail.ru>
2025-05-29 19:46:47 +0300

    1 commit

---
+++ src/test/java/FelineTest.java
@@ -0,0 +1,46 @@
+import com.example.Feline;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FelineTest {
+
+    @Test
+    public void testEatMeat() throws Exception {
+        Feline feline = new Feline();
+        List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
+        List<String> actualFood = feline.eatMeat();
+        assertEquals(expectedFood, actualFood);
+    }
+
+    @Test
+    public void testGetFamily() {
+        Feline feline = new Feline();
+        String expectedFamily = "Кошачьи";
+        String actualFamily = feline.getFamily();
+        assertEquals(expectedFamily, actualFamily);
+    }
+
+    @Test
+    public void testGetKittensWithoutParameters() {
+        Feline feline = new Feline();
+        int expectedKittens = 1;
+        int actualKittens = feline.getKittens();
+        assertEquals("1 котенок по умолчанию",
+                expectedKittens, actualKittens);
+    }
+
+    @Test
+    public void testGetKittensWithParameters() {
+        Feline feline = new Feline();
+        int expectedKittens = 6;
+        int actualKittens = feline.getKittens(6);
+        assertEquals("возвращаться должно заданное количество котяток",
+                expectedKittens, actualKittens);
+    }
+}
---
+++ src/test/java/LionTest.java
@@ -0,0 +1,52 @@
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LionTest {
+
+    @Mock
+    Feline felineMock;
+
+    @Test
+    public void testMaleLionHasMane() throws Exception {
+        Lion lion = new Lion("Самец", felineMock);
+        assertTrue(lion.doesHaveMane());
+    }
+
+    @Test
+    public void testFemaleLionHasNoMane() throws Exception {
+        Lion lion = new Lion("Самка", felineMock);
+        assertFalse(lion.doesHaveMane());
+    }
+
+    @Test(expected = Exception.class)
+    public void testInvalidSexThrowsException() throws Exception {
+        new Lion("Неизвестный пол", felineMock);
+    }
+
+    @Test
+    public void testGetKittens() throws Exception {
+        Lion lion = new Lion("Самец", felineMock);
+        when(felineMock.getKittens()).thenReturn(3);
+        assertEquals(3, lion.getKittens());
+        verify(felineMock).getKittens();
+    }
+
+    @Test
+    public void testGetFood() throws Exception {
+        Lion lion = new Lion("Самка", felineMock);
+        List<String> expectedFood = List.of("Мясо", "Рыба");
+        when(felineMock.getFood("Хищник")).thenReturn(expectedFood);
+        assertEquals(expectedFood, lion.getFood());
+        verify(felineMock).getFood("Хищник");
+    }
+}
